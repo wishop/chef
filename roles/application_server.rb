6682a83357fa0ba05cfb77b7d8792b31e7b15b36
Paulo Monteiro <paulo.from.portugal@gmail.com>
2013-03-21 22:10:42 +0000

    Reverting to openSDK v7

--- roles/application_server.rb
+++ roles/application_server.rb
@@ -10,12 +10,12 @@ run_list(all_env)
 
 override_attributes({
 
-  'java' => {
-	'oracle' => {
-		"accept_oracle_download_terms" => true
-	},
-	'jdk_version' => '7',
-	'install_flavor' => 'oracle'
+  #'java' => {
+  #	'oracle' => {
+  #		"accept_oracle_download_terms" => true
+  #	},
+	'jdk_version' => '7'
+  #	'install_flavor' => 'oracle'
   }
 })
 # Useful documentation:
